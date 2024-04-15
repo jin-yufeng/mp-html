ed87f101f1e509bc47768dd9a5568e7d1014d1dc
Jin Yufeng <1750665@tongji.edu.cn>
2024-04-15 20:54:02 +0800

    latex插件支持块级公式 #582

--- plugins/latex/index.js
+++ plugins/latex/index.js
@@ -10,27 +10,67 @@ function Latex () {
 
 Latex.prototype.onParse = function (node, vm) {
   // $...$包裹的内容为latex公式
-  if (!vm.options.editable && node.type === 'text' && /\$(.+?)\$/.test(node.text)) {
-    delete node.type
-    node.name = 'span'
-    node.attrs = {}
-    node.children = node.text.split('$').map((str, index) => {
-      // 偶数
-      if ((index + 1) % 2 === 0) {
-        return {
-          name: 'span',
-          attrs: {},
-          l: 'T',
-          f: 'display:inline-block',
-          children: parse.default(str)
+  if (!vm.options.editable && node.type === 'text' && node.text.includes('$')) {
+    const part = node.text.split(/(\${1,2})/)
+    const children = []
+    let status = 0
+    for (let i = 0; i < part.length; i++) {
+      if (i % 2 === 0) {
+        // 文本内容
+        if (part[i]) {
+          if (status === 0) {
+            children.push({
+              type: 'text',
+              text: part[i]
+            })
+          } else {
+            const nodes = parse.default(part[i])
+            if (status === 1) {
+              // 行内公式
+              children.push({
+                name: 'span',
+                attrs: {},
+                l: 'T',
+                f: 'display:inline-block',
+                children: nodes
+              })
+            } else {
+              // 块公式
+              children.push({
+                name: 'div',
+                attrs: {
+                  style: 'text-align:center'
+                },
+                children: nodes
+              })
+            }
+          }
+        }
+      } else {
+        // 分隔符
+        if (part[i] === '$' && part[i + 2] === '$') {
+          // 行内公式
+          status = 1
+          part[i + 2] = ''
+        } else if (part[i] === '$$' && part[i + 2] === '$$') {
+          // 块公式
+          status = 2
+          part[i + 2] = ''
+        } else {
+          if (part[i] && part[i] !== '$$') {
+            // 普通$符号
+            part[i + 1] = part[i] + part[i + 1]
+          }
+          // 重置状态
+          status = 0
         }
       }
-      return {
-        type: 'text',
-        text: str
-      }
-    }).filter(node => node.name || node.text)
-    delete node.text
+    }
+    node.type = undefined
+    node.text = undefined
+    node.name = 'span'
+    node.attrs = {}
+    node.children = children
   }
 }
 
